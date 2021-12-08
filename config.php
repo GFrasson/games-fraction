18a907ad0c8cca9f31545bd0bed2c238fcc32766
Fernando Giardini <fernandogiardining@outlook.com>
2021-12-08 17:54:06 -0300

    1 modificação no config e o dup do autoload do composer

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'nome_do_banco',
+        'name' => 'crud',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
